bca9ba4aaf874c5c52afc24cc7f4d612438fbab4
Anderson Felipe <anderson_fpv@yahoo.com.br>
2022-01-04 20:07:41 -0300

    Logando com e-mail e senha

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -36,6 +36,11 @@ export function SignIn() {
       });
   }
 
+  async function handleSignInWithEmailAndPassword(){
+    const { user } = await auth().signInWithEmailAndPassword(email, password);
+    console.log(user);
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -53,7 +58,7 @@ export function SignIn() {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" onPress={handleSignInAnonymously} />
+      <Button title="Entrar" onPress={handleSignInWithEmailAndPassword} />
 
       <Account>
         <ButtonText title="Recuperar senha" onPress={() => {}} />
